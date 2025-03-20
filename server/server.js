c7f5a7db78bbdc83a5e00ef92bd3fde50a284c3f
Adithya_G <adithyaacharya710@gmail.com>
2025-03-20 14:21:20 +0530

    updated cors origin for new client

--- server/server.js
+++ server/server.js
@@ -26,7 +26,7 @@ main();
 app.use(express.urlencoded({extended: true}));
 app.use(express.json()); 
 app.use(cors({
-    origin: "https://nochat-iev2.onrender.com",
+    origin: "https://nochat-iqi1.onrender.com",
     credentials: true,
 }));
 
@@ -52,7 +52,7 @@ app.use("/connection", connectionRouter);
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "https://nochat-iev2.onrender.com", 
+        origin: "https://nochat-iqi1.onrender.com", 
         credentials: true,
     },
 });
